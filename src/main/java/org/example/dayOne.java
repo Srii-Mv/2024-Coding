926d8088f85951a1426f91c4142df6325a7381dd
Srii-Mv <srinivaskumar.mv30@gmail.com>
2024-01-02 21:35:44 +0530

    Test1

---
+++ src/main/java/org/example/dayOne.java
@@ -0,0 +1,29 @@
+package org.example;
+import java.io.*;
+import java.util.Scanner;
+public class dayOne {
+    // Java Program to Check if Given Integer is Odd or Even
+//test
+        // Main Driver Method
+        public static void main(String[] args)
+        {
+            // Declaring and initializing integer variable
+            int num = 13;
+
+            // Checking if number is even or odd number
+            // via remainder
+            if (num % 2 == 0) {
+
+                // If remainder is zero then this number is even
+                System.out.println("Entered Number is Even");
+            }
+
+            else {
+
+                // If remainder is not zero then this number is
+                // odd
+                System.out.println("Entered Number is Odd");
+            }
+        }
+
+}
