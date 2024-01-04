d688a3499d07a0935a5490cd0cf9e426094f5949
Srii-Mv <srinivaskumar.mv30@gmail.com>
2024-01-04 09:04:21 +0530

    Test1

---
+++ src/main/java/org/example/dayTwo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.*;
+
+    public class dayTwo {
+        public static void main(String args[])
+        {
+            Date current_Date = new Date();
+            //"Date" class
+            //"current_Date" is Date object
+
+            System.out.println(current_Date);
+            // print the time and date
+        }
+    }
+
